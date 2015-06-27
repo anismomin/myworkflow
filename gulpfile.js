a76cfa3739458f7e65e95649ba2c71d048a020d0
anismomin <anis_momin@hotmail.com>
2015-06-27 19:42:38 +0500

    work on gulp file

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'); 
+var gutil = require('gulp-util'); //  for console messages or errot
+var coffee = require('gulp-coffee'); // work with coffee script
+var concat = require('gulp-concat'); // concate file and git return one file
+
+
+var coffeeSources = ['components/coffee/tagline.coffee'];
+
+var jsSources = [
+	'components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'
+];
+
+// in this task we take coffe script file and pass throught coffe script plugin 
+// that check for error if any error found add to console.or return to same destination
+gulp.task('coffee', function(){
+	gulp.src(coffeeSources)
+	.pipe(coffee({ bare: true })
+		.on('error', gutil.log))
+	.pipe(gulp.dest('components/scripts'))
+});
+
+// this task get all js file from one destination and concate them together and return in development folder scripts.js
+gulp.task('js', function(){
+	gulp.src(jsSources)
+	.pipe(concat('scripts.js'))
+	.pipe(gulp.dest('builds/development/js'))
+});
