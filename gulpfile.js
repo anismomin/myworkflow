d8b90363046021ba2c21da24bf57974a5f8cc275
anismomin <anis_momin@hotmail.com>
2015-06-29 22:59:01 +0500

    Added Static Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 var gulp = require('gulp'); 
 var gutil = require('gulp-util'); //  for console messages or errot
 var coffee = require('gulp-coffee'); // work with coffee script
-var mustache = require('gulp-mustache');
+var mustache = require('mustache');
 var browserify = require('gulp-browserify');
 var concat = require('gulp-concat'); // concate file and git return one file
-
+var compass = require('gulp-compass');
+var connect = require('gulp-connect');
 
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+
+var scssSource = ['components/sass/style.scss'];
+
+var HtmlSource = ['builds/development/*.html'];
+
+var JsonSource = ['builds/development/js/*.json'];
 
 var jsSources = [
 	'components/scripts/rclick.js',
@@ -25,10 +32,51 @@ gulp.task('coffee', function(){
 	.pipe(gulp.dest('components/scripts'))
 });
 
+
+gulp.task('sass', function(){
+	gulp.src(scssSource)
+	.pipe(compass({ 
+		sass: 'components/sass',
+      	image: 'builds/development/images',
+      	style: 'expanded'
+	})
+	.on('error', gutil.log))
+	.pipe(gulp.dest('builds/development/css'))
+	.pipe(connect.reload())
+});
+
 // this task get all js file from one destination and concate them together and return in development folder scripts.js
 gulp.task('js', function(){
 	gulp.src(jsSources)
-	.pipe(concat('scripts.js'))
+	.pipe(concat('script.js'))
 	.pipe(browserify())
 	.pipe(gulp.dest('builds/development/js'))
+	.pipe(connect.reload())
 });
+
+gulp.task('html', function(){
+	gulp.src(HtmlSource)
+	.pipe(connect.reload())
+});
+
+gulp.task('json', function(){
+	gulp.src(JsonSource)
+	.pipe(connect.reload())
+});
+
+gulp.task('watch', function(){
+	gulp.watch( coffeeSources , ['coffee']);
+	gulp.watch( jsSources , ['js']);
+	gulp.watch( JsonSource , ['json']);
+	gulp.watch( HtmlSource , ['html']);
+	gulp.watch( 'components/sass/*.scss' , ['sass']);
+});
+
+gulp.task('connect', function(){
+	connect.server({
+		root: 'builds/development',
+		livereload: true
+	})
+});
+
+gulp.task('default', ['html', 'json', 'coffee', 'sass', 'js' ,'connect', 'watch'])
